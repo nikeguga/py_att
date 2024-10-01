ebadf55856b31b7b368316907b3d3ad4dc7986cf
Nikeguga <nikeguga@gmail.com>
2024-10-02 01:48:11 +0300

    под размещение4+env с бд

--- py_attestation_project/settings.py
+++ py_attestation_project/settings.py
@@ -1,5 +1,6 @@
 import os
 import environ
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -59,10 +60,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'py_attestation_project.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 AUTH_PASSWORD_VALIDATORS = []
