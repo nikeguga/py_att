4deba42a6e4f312df0a44d4197a39e8df2aafbaa
Nikeguga <nikeguga@gmail.com>
2024-10-02 01:00:09 +0300

    под размещение

--- py_attestation_project/settings.py
+++ py_attestation_project/settings.py
@@ -2,11 +2,11 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'your-secret-key'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
