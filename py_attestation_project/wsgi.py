1859db9fdc068317a7268a96fc9a7d3a2bc61409
Nikeguga <nikeguga@gmail.com>
2024-10-02 06:11:03 +0300

    despair,THE ultimate despair

--- py_attestation_project/wsgi.py
+++ py_attestation_project/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'py_attestation_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'py_attestation_project.settings'))
 
 application = get_wsgi_application()
