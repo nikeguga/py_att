89d37884fd8176a9ba2ee45de91a98e109ebbfcf
Nikeguga <nikeguga@gmail.com>
2024-10-02 05:59:41 +0300

    despair,THE ultimate despair

--- py_attestation_project/settings.py
+++ py_attestation_project/settings.py
@@ -85,3 +85,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
